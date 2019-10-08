a70537351a4197356d4b95c999b4f8344ad08906
kennymaina <victormurigi@outlook.com>
2019-10-08 13:04:54 +0300

    addition of newsreceipients list where the admin can view the people who has subscribed to our newsletter

--- certeon/admin.py
+++ certeon/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import NewsUsers
+# from .models import NewsUsers
+from .models import NewsLetterRecipients
+
+
 
 # Register your models here.
-admin.site.register(NewsUsers)
+# admin.site.register(NewsUsers)
+admin.site.register(NewsLetterRecipients)
+
