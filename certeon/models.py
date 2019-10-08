b2d8613286587deebd5b656c051cc28286bd8c49
kennymaina <victormurigi@outlook.com>
2019-10-08 13:07:17 +0300

    subscription form functionality

--- certeon/models.py
+++ certeon/models.py
@@ -3,14 +3,17 @@ from django.db import models
 
 
 
-class NewsUsers(models.Model):
+# class NewsUsers(models.Model):
+#     name = models.CharField(max_length = 30)
+#     email = models.EmailField()
+#     date_added = models.DateField(auto_now_add=True)
+#     class Meta:
+#         verbose_name = 'NewsUser'
+#         verbose_name_plural = 'NewsUsers'
+#         def __str__(self):
+#             return self.email
+
+
+class NewsLetterRecipients(models.Model):
     name = models.CharField(max_length = 30)
     email = models.EmailField()
-    date_added = models.DateField(auto_now_add=True)
-    class Meta:
-        verbose_name = 'NewsUser'
-        verbose_name_plural = 'NewsUsers'
-        def __str__(self):
-            return self.email
-
-
